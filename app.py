4fd4eda8b295be41dd778cceaf899a264a84861c
harry.housser@menloschool.org <harry.housser@menloschool.org>
2023-05-03 11:54:03 -0700

    got app.py working

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ def signup():
 def login():
     return render_template("login.html")
 
-@app.before_first_request
-def setup():
-    init_db()
+
+    
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
 
